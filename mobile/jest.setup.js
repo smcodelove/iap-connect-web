0092862abda434533bef33db86a299c8bceb5818
Shivam Yadav <shivamyadav@Shivams-MacBook-Air.local>
2025-08-02 15:18:50 +0530

    Initial commit

--- mobile/jest.setup.js
+++
@@ -1,9 +0,0 @@
-if (typeof window === 'undefined') {
-  global.window = {};
-}
-if (typeof document === 'undefined') {
-  global.document = {};
-}
-if (typeof navigator === 'undefined') {
-  global.navigator = {};
-}
